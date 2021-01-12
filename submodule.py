5d6fb6991489925bf478fece50e8f80252d486ae
Ido <idogada@agatsoftware.com>
2021-01-12 09:06:22 +0200

    Version from variable

--- submodule.py
+++ submodule.py
@@ -2,13 +2,13 @@ import json
 from flask import Blueprint
 
 submodule = Blueprint('submodule', __name__)
-version = 0.1
+version = '0.0.1'
 
 @submodule.route('/')
 def default():
     global version
     returnDict = {
         'name': __name__,
-        'version':0.1
+        'version':version
     }
     return json.dumps(returnDict, indent=4, sort_keys=True)
