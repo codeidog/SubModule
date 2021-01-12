cf6fcea5247686951de30545c7aa0adc491c85ec
Ido <idogada@agatsoftware.com>
2021-01-12 08:48:21 +0200

    Submodule blueprint

---
+++ submodule.py
@@ -0,0 +1,14 @@
+import json
+from flask import Blueprint
+
+submodule = Blueprint('submodule', __name__)
+version = 0.1
+
+@submodule.route('/')
+def default():
+    global version
+    returnDict = {
+        'name': __name__,
+        'version':0.1
+    }
+    return json.dumps(returnDict, indent=4, sort_keys=True)
